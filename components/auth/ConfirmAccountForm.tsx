98d4ee66c00c79a6b62d3dc9d50d50b2e3175beb
samuel <samusplay08@gmail.com>
2026-07-14 12:32:43 -0500

    handle con eventos

--- components/auth/ConfirmAccountForm.tsx
+++ components/auth/ConfirmAccountForm.tsx
@@ -1,12 +1,36 @@
 "use client"
 
 import { PinInput, PinInputField } from "@chakra-ui/pin-input"
+import { useState } from "react"
 
 export default function ConfirmAccountForm(){
+    //usestate para guardar el estado
+    const [token,setToken]=useState("")
+
+    //funcion manejadora guardar el token
+    const handleChange=(token:string)=>{
+        //set almacena datos unicos es una estrctura de datos
+        setToken(token)
+       
+    }
+    //disparamos un evento en automatico
+    //mandar a llamar el server action y comprobarlo
+    const handleComplete=()=>{
+       
+
+    }
+
+
+
     //Utilizamos libreria Chakra
     return(
         <div className='flex justify-center gap-5 my-10'>
-            <PinInput>
+            <PinInput
+            value={token}
+            onChange={handleChange}
+            onComplete={handleComplete}
+            
+            >
                 <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
                 <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
                 <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
