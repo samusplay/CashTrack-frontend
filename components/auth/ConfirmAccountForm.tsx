a961ef7ab56f5ab82f86f795fe08b33c20a77cba
samuel <samusplay08@gmail.com>
2026-07-14 12:17:29 -0500

    agregadno libreria chackra

---
+++ components/auth/ConfirmAccountForm.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { PinInput, PinInputField } from "@chakra-ui/pin-input"
+
+export default function ConfirmAccountForm(){
+    //Utilizamos libreria Chakra
+    return(
+        <div className='flex justify-center gap-5 my-10'>
+            <PinInput>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+                <PinInputField  className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+            </PinInput>
+        </div>
+    )
+}
